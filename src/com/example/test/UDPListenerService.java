95d1f676edf8315b1b46d7869545e613cd33d6f2
Prathamesh Karve <prathamesh.karve@gmail.com>
2013-06-01 18:37:30 +0530

    packets not received fixed
    acquired multicast lock before receive

--- src/com/example/test/UDPListenerService.java
+++ src/com/example/test/UDPListenerService.java
@@ -8,8 +8,11 @@ import java.net.UnknownHostException;
  
 import org.apache.http.util.ExceptionUtils;
  
+import android.app.Activity;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
@@ -18,7 +21,7 @@ import android.widget.Toast;
  
 
 public class UDPListenerService extends Service {
-  static String UDP_BROADCAST = "UDPBroadcast";
+	static String UDP_BROADCAST = "UDPBroadcast";
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
@@ -33,7 +36,7 @@ public class UDPListenerService extends Service {
 			public void run() {
 				try {
 					Log.d("UDP", "Inside try");
-					InetAddress broadcastIP = InetAddress.getByName("192.168.1.9"); //172.16.238.42 //192.168.1.255
+					InetAddress broadcastIP = InetAddress.getByName("127.0.0.1"); //172.16.238.42 //192.168.1.255
 					Integer port = MainActivity2.SERVERPORT;
 					while (shouldRestartSocketListen) {
 						Log.d("UDP", "Inside while");
@@ -57,8 +60,9 @@ public class UDPListenerService extends Service {
 		byte[] recvBuf = new byte[15000];
 		if (socket == null || socket.isClosed()) {
 			Log.d("UDP", "Inside if");
+			//socket.setReuseAddress(true);
+			socket = new DatagramSocket(port);
 			socket.setReuseAddress(true);
-			socket = new DatagramSocket(MainActivity2.SERVERPORT, InetAddress.getByName("192.168.1.9"));
 			Log.d("UDP", "Socket port and address set");
 			socket.setBroadcast(true);
 		}
@@ -66,14 +70,20 @@ public class UDPListenerService extends Service {
 		DatagramPacket packet = new DatagramPacket(recvBuf, recvBuf.length);
 		Log.e("UDP", "Waiting for UDP broadcast");
 		//socket.receive(packet);
-		updatetrack("Server: Receiving\n");
+		//updatetrack("Gotya Server: Receiving\n");
+		WifiManager mWifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+		WifiManager.MulticastLock multicastLock = mWifiManager.createMulticastLock("net.inside.broadcast");
+		multicastLock.setReferenceCounted(true);
+		multicastLock.acquire();
         socket.receive(packet);
-        updatetrack("Server: Message received: '" + new String(packet.getData()) + "'\n");
-        updatetrack("Server: Succeed!\n");
+        //updatetrack("Gotya Server: Message received: '" + new String(packet.getData()) + "'\n");
+        //updatetrack("Gotya Server: Succeed!\n");
 		
 		String senderIP = packet.getAddress().getHostAddress();
 		socket.close();
-		Log.e("UDP", "Got UDB broadcast from " + senderIP + ", message: " + new String(packet.getData()));
+		updatetrack(senderIP + ": " + new String(packet.getData())+"\n");
+		Log.d("UDP", "Got UDB broadcast from " + senderIP + ", message: " + new String(packet.getData()));
+		
  
 		//broadcastIntent(senderIP, message);
 		
